37a4f44c17d164bc4b4db4e9963280a5c266873f
zonary123 <carlosvarasalonso10@gmail.com>
2025-10-01 21:26:54 +0200

    enhance Currency initialization with additional currency identifiers and update currency IDs for consistency

--- src/main/java/com/kingpixel/ultraeconomy/config/Currencies.java
+++ src/main/java/com/kingpixel/ultraeconomy/config/Currencies.java
@@ -23,12 +23,16 @@ public class Currencies {
     folder.mkdirs();
     var files = Utils.getFiles(folder);
     if (files.isEmpty()) {
-      Currency currency = new Currency(true, (byte) 2, "$");
-      currency.setId("dollar");
+      Currency currency = new Currency(true, (byte) 2, "$", new String[]{
+        "impactor:dollars"
+      });
+      currency.setId("dollars");
       CURRENCY_MAP.put(currency.getId(), currency);
       writeCurrency(currency);
-      Currency currency2 = new Currency(false, (byte) 2, "€");
-      currency2.setId("euro");
+      Currency currency2 = new Currency(false, (byte) 2, "€", new String[]{
+        "impactor:tokens"
+      });
+      currency2.setId("tokens");
       CURRENCY_MAP.put(currency2.getId(), currency2);
       writeCurrency(currency2);
     } else {
